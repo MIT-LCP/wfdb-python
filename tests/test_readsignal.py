825ae1871d460e7c4d0bb296beb86a718216aeed
Tom Pollard <tpollard@mit.edu>
2016-06-06 21:05:17 -0400

    add unit tests for format_16. ref #5

---
+++ tests/test_readsignal.py
@@ -0,0 +1,32 @@
+import numpy as np
+from wfdb import readsignal
+
+# NB: write np.array to CSV with:
+# sig[:,1].tofile('test01_00s_channel1.csv',sep=',',format='%s')
+
+class test_read_format_16():
+
+    def setUp(self):
+        # load the sample binary file
+        self.sig, self.fields = readsignal.rdsamp('sampledata/test01_00s')
+
+    def test_channel1(self):
+        # load the expected results
+        channel1 = np.genfromtxt('tests/testdata/test01_00s_channel1.csv', delimiter=',')
+        assert np.array_equal(self.sig[:,0],channel1)
+
+    def test_channel2(self):
+        # load the expected results
+        channel2 = np.genfromtxt('tests/testdata/test01_00s_channel2.csv', delimiter=',')
+        assert np.array_equal(self.sig[:,1],channel2)
+
+    def test_channel3(self):
+        # load the expected results
+        channel3 = np.genfromtxt('tests/testdata/test01_00s_channel3.csv', delimiter=',')
+        assert np.array_equal(self.sig[:,2],channel3)
+    
+    def test_channel4(self):
+        # load the expected results
+        channel4 = np.genfromtxt('tests/testdata/test01_00s_channel4.csv', delimiter=',')
+        assert np.array_equal(self.sig[:,3],channel4)
+
