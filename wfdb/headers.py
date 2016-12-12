cef6deef6302a2d1842983d8953417525e50d65b
Chen <is.chenx@gmail.com>
2016-12-12 15:19:37 -0500

    restructuring modules

---
+++ wfdb/headers.py
@@ -0,0 +1,268 @@
+import numpy as np
+import re
+import os
+import sys
+import requests
+
+# The information contained in a WFDB header file
+WFDBfields = {
+        'recordname': [],
+        'nseg': [],
+        'nsig': [],
+        'fs': [],
+        'nsamp': [],
+        'basetime': [],
+        'basedate': [],
+        'filename': [],
+        'fmt': [],
+        'sampsperframe': [],
+        'skew': [],
+        'byteoffset': [],
+        'gain': [],
+        'units': [],
+        'baseline': [],
+        'initvalue': [],
+        'signame': [],
+        'nsampseg': [],
+        'comments': []}
+
+
+
+
+
+# The required explicit fields contained in all WFDB headers. 
+# The second sublist is only required in headers with signal channels. 
+req_read_fields = [['recordname', 'nsig'], ['filename', 'fmt']]
+
+# The required input fields used to write WFDB headers. Module does not assume fs=250Hz as when reading headers. 
+# The second sublist is only required for writing headers with signal channels.  
+req_write_fields = [['recordname', 'nsig', 'fs'], ['filename', 'fmt']]
+
+# Write a wfdb header file
+def wrheader(inputfields):
+    
+   
+    # Check that the input fields are valid
+    errors = checkheaderfields(inputfields)
+    
+    if errors:
+        print("The following errors were detected with the input fields:")
+        for error in errors:
+            print(error)
+        sys.exit()
+        
+    
+    
+# Check that a dictionary of fields contains valid keys/values according to WFDB standards.
+def checkheaderfields(inputfields):
+    
+    errors = []
+    
+    # Check for invalid dictionary keys
+    for inputfield in inputfields:
+        if inputfield not in list():
+            errors.append('Invalid input key: '+inputfield)
+    
+    # Check for mandatory input fields
+    for req_field in req_write_fields[0]:
+        if req_field not in list(inputfields):
+            errors.append('Missing required input key: '+req_field)
+    if nsig>0:
+        for req_field in req_write_fields[1]:
+            if req_field not in list(inputfields):
+                errors.append('Missing required input key: '+req_field)
+    
+    # Check the validity of each dictionary value
+    for field in inputfields:
+        valueerror=checkheadervalue(field, inputfields[field])
+        if valueerror:
+            errors.append(valueerror)
+    
+    return errors
+    
+    
+def checkheadervalue(field, value, nsig)
+    
+    # These fields must have string values
+    stringfields = []
+    
+    
+    
+    # These 
+
+    if field=='recordname':
+        break
+    
+    return error
+    
+    
+    
+    
+    
+    
+
+
+def rdheader(recordname):  # For reading signal headers
+
+    # To do: Allow exponential input format for some fields
+
+    # Output dictionary
+    fields=WFDBfields
+    
+    # filename stores file names for both multi and single segment headers.
+    # nsampseg is only for multi-segment
+
+
+    # RECORD LINE fields (o means optional, delimiter is space or tab unless specified):
+    # record name, nsegments (o, delim=/), nsignals, fs (o), counter freq (o, delim=/, needs fs),
+    # base counter (o, delim=(), needs counter freq), siglen (o, needs fs), base time (o),
+    # base date (o needs base time).
+
+    # Regexp object for record line
+    rxRECORD = re.compile(
+        ''.join(
+            [
+                "(?P<name>[\w]+)/?(?P<nseg>\d*)[ \t]+",
+                "(?P<nsig>\d+)[ \t]*",
+                "(?P<fs>\d*\.?\d*)/*(?P<counterfs>\d*\.?\d*)\(?(?P<basecounter>\d*\.?\d*)\)?[ \t]*",
+                "(?P<siglen>\d*)[ \t]*",
+                "(?P<basetime>\d*:?\d{,2}:?\d{,2}\.?\d*)[ \t]*",
+                "(?P<basedate>\d{,2}/?\d{,2}/?\d{,4})"]))
+    # Watch out for potential floats: fs (and also exponent notation...),
+    # counterfs, basecounter
+
+    # SIGNAL LINE fields (o means optional, delimiter is space or tab unless specified):
+    # file name, format, samplesperframe(o, delim=x), skew(o, delim=:), byteoffset(o,delim=+),
+    # ADCgain(o), baseline(o, delim=(), requires ADCgain), units(o, delim=/, requires baseline),
+    # ADCres(o, requires ADCgain), ADCzero(o, requires ADCres), initialvalue(o, requires ADCzero),
+    # checksum(o, requires initialvalue), blocksize(o, requires checksum),
+    # signame(o, requires block)
+
+    # Regexp object for signal lines. Consider flexible filenames, and also ~
+    rxSIGNAL = re.compile(
+        ''.join(
+            [
+                "(?P<filename>[\w]*\.?[\w]*~?)[ \t]+(?P<format>\d+)x?"
+                "(?P<sampsperframe>\d*):?(?P<skew>\d*)\+?(?P<byteoffset>\d*)[ \t]*",
+                "(?P<ADCgain>-?\d*\.?\d*e?[\+-]?\d*)\(?(?P<baseline>-?\d*)\)?/?(?P<units>[\w\^/-]*)[ \t]*",
+                "(?P<ADCres>\d*)[ \t]*(?P<ADCzero>-?\d*)[ \t]*(?P<initialvalue>-?\d*)[ \t]*",
+                "(?P<checksum>-?\d*)[ \t]*(?P<blocksize>\d*)[ \t]*(?P<signame>[\S]*)"]))
+
+    # Units characters: letters, numbers, /, ^, -,
+    # Watch out for potentially negative fields: baseline, ADCzero, initialvalue, checksum,
+    # Watch out for potential float: ADCgain.
+
+    # Read the header file and get the comment and non-comment lines
+    headerlines, commentlines = _getheaderlines(recordname)
+
+    # Get record line parameters
+    (_, nseg, nsig, fs, counterfs, basecounter, siglen,
+    basetime, basedate) = rxRECORD.findall(headerlines[0])[0]
+
+    # These fields are either mandatory or set to defaults.
+    if not nseg:
+        nseg = '1'
+    if not fs:
+        fs = '250'
+
+    fields['nseg'] = int(nseg)
+    fields['fs'] = float(fs)
+    fields['nsig'] = int(nsig)
+    fields['recordname'] = name
+
+    # These fields might by empty
+    if siglen:
+        fields['siglen'] = int(siglen)
+    fields['basetime'] = basetime
+    fields['basedate'] = basedate
+
+
+    # Signal or Segment line paramters
+    # Multi segment header - Process segment spec lines in current master
+    # header.
+    if int(nseg) > 1:
+        for i in range(0, int(nseg)):
+            (filename, nsampseg) = re.findall(
+                '(?P<filename>\w*~?)[ \t]+(?P<nsampseg>\d+)', headerlines[i + 1])[0]
+            fields["filename"].append(filename)
+            fields["nsampseg"].append(int(nsampseg))
+    # Single segment header - Process signal spec lines in regular header.
+    else:
+        for i in range(0, int(nsig)):  # will not run if nsignals=0
+            # get signal line parameters
+            (filename,
+            fmt,
+            sampsperframe,
+            skew,
+            byteoffset,
+            adcgain,
+            baseline,
+            units,
+            adcres,
+            adczero,
+            initvalue,
+            checksum,
+            blocksize,
+            signame) = rxSIGNAL.findall(headerlines[i + 1])[0]
+            
+            # Setting defaults
+            if not sampsperframe:
+                # Setting strings here so we can always convert strings case
+                # below.
+                sampsperframe = '1'
+            if not skew:
+                skew = '0'
+            if not byteoffset:
+                byteoffset = '0'
+            if not adcgain:
+                adcgain = '200'
+            if not baseline:
+                if not adczero:
+                    baseline = '0'
+                else:
+                    baseline = adczero  # missing baseline actually takes adczero value if present
+            if not units:
+                units = 'mV'
+            if not initvalue:
+                initvalue = '0'
+            if not signame:
+                signame = "ch" + str(i + 1)
+            if not initvalue:
+                initvalue = '0'
+
+            fields["filename"].append(filename)
+            fields["fmt"].append(fmt)
+            fields["sampsperframe"].append(int(sampsperframe))
+            fields["skew"].append(int(skew))
+            fields['byteoffset'].append(int(byteoffset))
+            fields["gain"].append(float(adcgain))
+            fields["baseline"].append(int(baseline))
+            fields["units"].append(units)
+            fields["initvalue"].append(int(initvalue))
+            fields["signame"].append(signame)
+
+    for comment in commentlines:
+        fields["comments"].append(comment.strip('\s#'))
+
+    return fields
+
+
+# Read header file to get comment and non-comment lines
+def _getheaderlines(recordname):
+    with open(recordname + ".hea", 'r') as fp:
+        headerlines = []  # Store record line followed by the signal lines if any
+        commentlines = []  # Comments
+        for line in fp:
+            line = line.strip()
+            if line.startswith('#'):  # comment line
+                commentlines.append(line)
+            elif line:  # Non-empty non-comment line = header line.
+                ci = line.find('#')
+                if ci > 0:
+                    headerlines.append(line[:ci])  # header line
+                    # comment on same line as header line
+                    commentlines.append(line[ci:])
+                else:
+                    headerlines.append(line)
+    return headerlines, commentlines
+
