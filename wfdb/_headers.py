a088cd576807d9cc09f50e8e08aa483fd7608a44
Chen <is.chenx@gmail.com>
2017-02-07 15:27:14 -0500

    merge rdheader into _headers.py

--- wfdb/_headers.py
+++ wfdb/_headers.py
@@ -78,6 +78,9 @@ class HeadersMixin(HeadersBaseMixin):
                 self.adcres = self.nsig*[0]
             else:
                 self.adcres=_signals.wfdbfmtres(self.fmt)
+
+                # For all of these, including this, just use the default defined static field. THEN check for other like wfdbfmtres.... 
+
         elif field == 'adczero':
             self.adczero = self.nsig*[0]
         elif field == 'blocksize':
@@ -180,7 +183,6 @@ class HeadersMixin(HeadersBaseMixin):
 # Class with multi-segment header methods
 # To be inherited by WFDBmultirecord from records.py.
 class MultiHeadersMixin(BaseHeadersMixin):
-
     
     # Get the list of fields used to write the multi-segment header. 
     # Returns the default required fields, the user defined fields, and their dependencies.
@@ -261,57 +263,186 @@ class MultiHeadersMixin(BaseHeadersMixin):
         linestofile(self.recordname+'.hea', headerlines)
 
 
+# Regexp objects for reading headers
 
-# Specifications of WFDB header fields.
-class WFDBheaderspecs():
-    
-    def __init__(self, speclist):
-        # Data types the field (or its elements) can be
-        self.allowedtypes = speclist[0]
-        # The text delimiter that preceeds the field if it is a field that gets written to header files.
-        self.delimiter = speclist[1]
-        # The required/dependent field which must also be present
-        self.dependency = speclist[2]
-        # Whether the field is always required for writing a header (WFDB requirements + extra rules enforced by this library).
-        self.write_req = speclist[3]
+# Record Line Fields
+rxRECORD = re.compile(
+    ''.join(
+        [
+            "(?P<recordname>[\w]+)/?(?P<nseg>\d*)[ \t]+",
+            "(?P<nsig>\d+)[ \t]*",
+            "(?P<fs>\d*\.?\d*)/*(?P<counterfs>\d*\.?\d*)\(?(?P<basecounter>\d*\.?\d*)\)?[ \t]*",
+            "(?P<siglen>\d*)[ \t]*",
+            "(?P<basetime>\d*:?\d{,2}:?\d{,2}\.?\d*)[ \t]*",
+            "(?P<basedate>\d{,2}/?\d{,2}/?\d{,4})"]))
+
+# Signal Line Fields
+rxSIGNAL = re.compile(
+    ''.join(
+        [
+            "(?P<filename>[\w]*\.?[\w]*~?)[ \t]+(?P<fmt>\d+)x?"
+            "(?P<sampsperframe>\d*):?(?P<skew>\d*)\+?(?P<byteoffset>\d*)[ \t]*",
+            "(?P<adcgain>-?\d*\.?\d*e?[\+-]?\d*)\(?(?P<baseline>-?\d*)\)?/?(?P<units>[\w\^/-]*)[ \t]*",
+            "(?P<adcres>\d*)[ \t]*(?P<adczero>-?\d*)[ \t]*(?P<initvalue>-?\d*)[ \t]*",
+            "(?P<checksum>-?\d*)[ \t]*(?P<blocksize>\d*)[ \t]*(?P<signame>[\S]*)"]))
+
+# Segment Line Fields
+rxSEGMENT = re.compile('(?P<segname>\w*~?)[ \t]+(?P<seglen>\d+)')
+
+# Read a WFDB header. Return a WFDBrecord object or WFDBmultirecord object
+def rdheader(recordname):  
+
+    # Read the header file. Separate comment and non-comment lines
+    headerlines, commentlines = getheaderlines(recordname)
+
+    # Get fields from record line
+    d_rec = read_record_line(headerlines[0])
+
+    # Processing according to whether the header is single or multi segment
+
+    # Single segment header - Process signal specification lines
+    if nseg is None:
+        # Create a single-segment WFDB record object
+        record = WFDBrecord()
+        # Read the fields from the signal lines
+        d_sig = read_sig_lines(headerlines[1:])
+        # Set the object's fields
+        for field in sigfieldspecs:
+            setattr(record, field, d_sig[field])    
+    # Multi segment header - Process segment specification lines
+    else:
+        # Create a multi-segment WFDB record object
+        record = WFDBmultirecord()
+        # Read the fields from the segment lines
+        d_seg = read_seg_lines(headerlines[1:])    
+        # Set the object's fields
+        for field in segfieldspecs:
+            setattr(record, field, d_seg[field])  
+
+    # Set the comments field
+    record.comments = []
+    for line in commentlines:
+        record.comments.append(line.strip('\s#'))
+
+    # Set the record line fields
+    for field in recfieldspecs:
+        setattr(record, field, d_rec[field])
+
+    return record
 
 
-# Record specification fields  
-# Note: nseg is essential for multi but not a field in single. 
-# getwritefields defined in _headers.Headers_Mixin will remove it.           
-recfieldspecs = OrderedDict([('recordname', WFDBheaderspecs([[str], '', None, True])),
-                         ('nseg', WFDBheaderspecs([[int, np.int64, np.int32], '/', 'recordname', True])), 
-                         ('nsig', WFDBheaderspecs([[int, np.int64, np.int32], ' ', 'recordname', True])),
-                         ('fs', WFDBheaderspecs([[int, float, np.float64, np.float32], ' ', 'nsig', True])),
-                         ('counterfreq', WFDBheaderspecs([[int, np.int64, np.int32, float, np.float64, np.float32], '/', 'fs', False])),
-                         ('basecounter', WFDBheaderspecs([[int, np.int64, np.int32, float, np.float64, np.float32], '(', 'counterfreq', False])),
-                         ('siglen', WFDBheaderspecs([[int, np.int64, np.int32], ' ', 'fs', True])),
-                         ('basetime', WFDBheaderspecs([[str], ' ', 'siglen', False])),
-                         ('basedate', WFDBheaderspecs([[str], ' ', 'basetime', False]))])
- 
-# Signal specification fields. Type will be list.
-sigfieldspecs = OrderedDict([('filename', WFDBheaderspecs([[str], '', None, True])),
-                         ('fmt', WFDBheaderspecs([[str], ' ', 'filename', True])),
-                         ('sampsperframe', WFDBheaderspecs([[int, np.int64, np.int32], 'x', 'fmt', False])),
-                         ('skew', WFDBheaderspecs([[int, np.int64, np.int32], ':', 'fmt', False])),
-                         ('byteoffset', WFDBheaderspecs([[int, np.int64, np.int32], '+', 'fmt', False])),
-                         ('adcgain', WFDBheaderspecs([[int, np.int64, np.int32, float, np.float64, np.float32], ' ', 'fmt', True])),
-                         ('baseline', WFDBheaderspecs([[int, np.int64, np.int32], '(', 'adcgain', True])),
-                         ('units', WFDBheaderspecs([[str], '/', 'adcgain', True])),
-                         ('adcres', WFDBheaderspecs([[int, np.int64, np.int32], ' ', 'adcgain', False])),
-                         ('adczero', WFDBheaderspecs([[int, np.int64, np.int32], ' ', 'adcres', False])),
-                         ('initvalue', WFDBheaderspecs([[int, np.int64, np.int32], ' ', 'adczero', False])),
-                         ('checksum', WFDBheaderspecs([[int, np.int64, np.int32], ' ', 'initvalue', False])),
-                         ('blocksize', WFDBheaderspecs([[int, np.int64, np.int32], ' ', 'checksum', False])),
-                         ('signame', WFDBheaderspecs([[str], ' ', 'blocksize', False]))])
-    
-# Segment specification fields. Type will be list. 
-segfieldspecs = OrderedDict([('segname', WFDBheaderspecs([[str], '', None, True])),
-                         ('seglen', WFDBheaderspecs([[int, np.int64, np.int32], ' ', 'segname', True]))])
+# Read header file to get comment and non-comment lines
+def getheaderlines(recordname):
+    with open(recordname + ".hea", 'r') as fp:
+        # Record line followed by signal/segment lines if any
+        headerlines = []  
+        # Comment lines
+        commentlines = []  
+        for line in fp:
+            line = line.strip()
+            # Comment line
+            if line.startswith('#'):  
+                commentlines.append(line)
+            # Non-empty non-comment line = header line.
+            elif line:  
+                ci = line.find('#')
+                if ci > 0:
+                    headerlines.append(line[:ci])  # header line
+                    # comment on same line as header line
+                    commentlines.append(line[ci:])
+                else:
+                    headerlines.append(line)
+    return headerlines, commentlines
 
 
+# Extract fields from a record line string into a dictionary
+def read_rec_line(recline):
+
+    # Dictionary for record fields
+    d_rec = {}
+
+    # Read string fields from record line
+    (d_rec['recordname'], d_rec['nseg'], d_rec['nsig'], d_rec['fs'], 
+    d_rec['counterfs'], d_rec['basecounter'], d_rec['siglen'],
+    d_rec['basetime'], d_rec['basedate']) = re.findall(rxRECORD, recordline)[0]
+
+    for field in recfieldspecs:
+        # Replace empty strings with their read defaults (which are mostly None)
+        if d_rec[field] == '':
+            d_rec[field] = recfieldspecs[field].read_def
+        # Typecast non-empty strings for numerical fields
+        else:
+            if recfieldspecs[field].allowedtypes is inttypes:
+                d_rec[field] = int(d_rec[field])
+            elif recfieldspecs[field].allowedtypes is floattypes:
+                d_rec[field] = float(d_rec[field])
+
+    return d_rec
+
+# Extract fields from signal line strings into a dictionary
+def read_sig_lines(siglines):
+
+    # Dictionary for signal fields
+    d_sig = {}
+
+    # Each dictionary field is a list
+    for field in sigfieldspecs:
+        d_sig[field] = [None]*len(siglines)
+
+    # Read string fields from signal line
+    for i in range(0, len(siglines)):
+        (d_sig['filename'][i], d_sig['fmt'][i],
+            d_sig['sampsperframe'][i],
+            d_sig['skew'][i],
+            d_sig['byteoffset'][i],
+            d_sig['adcgain'][i],
+            d_sig['baseline'][i],
+            d_sig['units'][i],
+            d_sig['adcres'][i],
+            d_sig['adczero'][i],
+            d_sig['initvalue'][i],
+            d_sig['checksum'][i],
+            d_sig['blocksize'][i],
+            d_sig['signame'][i]) = rxSIGNAL.findall(siglines[i])[0]
+
+        for field in sigfieldspecs:
+            # Replace empty strings with their read defaults (which are mostly None)
+            if d_sig[field][i] == '':
+                d_sig[field][i] = sigfieldspecs[field].read_def
+            # Typecast non-empty strings for numerical fields
+            else:
+                if sigfieldspecs[field].allowedtypes is inttypes:
+                    d_sig[field][i] = int(d_sig[field][i])
+                elif sigfieldspecs[field].allowedtypes is floattypes:
+                    d_sig[field][i] = float(d_sig[field][i])
+
+    return d_sig
 
 
+# Extract fields from segment line strings into a dictionary
+def read_seg_lines(seglines):
+
+    # Dictionary for signal fields
+    d_seg = {}
+
+    # Each dictionary field is a list
+    for field in segfieldspecs:
+        d_seg[field] = [None]*len(seglines)
+
+    # Read string fields from signal line
+    for i in range(0, len(seglines)):
+        (d_seg['segname'][i], d_seg['seglen'][i]) = rxSEGMENT.findall(seglines[i])[0]
+
+        for field in segfieldspecs:
+            # Replace empty strings with their read defaults (which are mostly None)
+            if d_seg[field][i] == '':
+                d_seg[field][i] = segfieldspecs[field].read_def
+            # Typecast non-empty strings for numerical field
+            else:
+                if field == 'seglen'
+                    d_seg[field][i] = int(d_seg[field][i])
+                                 
+    return d_seg
 
 # Write each line in a list of strings to a text file
 def linestofile(filename, lines):
@@ -319,3 +450,68 @@ def linestofile(filename, lines):
     for l in lines:
         f.write("%s\n" % l)
     f.close()              
+
+
+# Specifications of WFDB header fields.
+class WFDBheaderspecs():
+    
+    def __init__(self, allowedtypes, delimiter, dependency, write_req, read_def, write_def):
+        # Data types the field (or its elements) can be
+        self.allowedtypes = allowedtypes
+        # The text delimiter that preceeds the field if it is a field that gets written to header files.
+        self.delimiter = delimiter
+        # The required/dependent field which must also be present
+        self.dependency = dependency
+        # Whether the field is always required for writing a header (more stringent than origin WFDB library)
+        self.write_req = write_req
+        # The default value for the field when read if any
+        self.read_def = read_def
+        # The default value for the field to fill in before writing if any
+        self.read_def = write_def
+
+        # The read vs write default values are different for 2 reasons:
+        # 1. We want to force the user to be explicit with certain important
+        #    fields when writing WFDB records fields, without affecting 
+        #    existing WFDB headers when reading.
+        # 2. Certain unimportant fields may be dependencies of other
+        #    important fields. When writing, we want to fill in defaults
+        #    so that the user doesn't need to. But when reading, it should
+        #    be clear that the fields are missing. 
+
+inttypes = [int, np.int64, np.int32]
+floattypes = inttypes + [float, np.float64, np.float32]
+
+# Record specification fields            
+recfieldspecs = OrderedDict([('recordname', WFDBheaderspecs([str], '', None, True, None, None)),
+                         ('nseg', WFDBheaderspecs(inttypes, '/', 'recordname', True, None, None)), 
+                         ('nsig', WFDBheaderspecs(inttypes, ' ', 'recordname', True, None, None)),
+                         ('fs', WFDBheaderspecs(floattypes, ' ', 'nsig', True, 250, None)),
+                         ('counterfreq', WFDBheaderspecs(floattypes, '/', 'fs', False, None, None)),
+                         ('basecounter', WFDBheaderspecs(floattypes, '(', 'counterfreq', False, None, None)),
+                         ('siglen', WFDBheaderspecs(inttypes, ' ', 'fs', True, None, None)),
+                         ('basetime', WFDBheaderspecs([str], ' ', 'siglen', False, None, '00:00:00')),
+                         ('basedate', WFDBheaderspecs([str], ' ', 'basetime', False, None, None))])
+ 
+# Signal specification fields.
+sigfieldspecs = OrderedDict([('filename', WFDBheaderspecs([str], '', None, True, None, None)),
+                         ('fmt', WFDBheaderspecs([str], ' ', 'filename', True, None, None)),
+                         ('sampsperframe', WFDBheaderspecs(inttypes, 'x', 'fmt', False, None)),
+                         ('skew', WFDBheaderspecs(inttypes, ':', 'fmt', False, None, None)),
+                         ('byteoffset', WFDBheaderspecs(inttypes, '+', 'fmt', False, None, None)),
+                         ('adcgain', WFDBheaderspecs(floattypes, ' ', 'fmt', True, 200, None)),
+                         ('baseline', WFDBheaderspecs(inttypes, '(', 'adcgain', True, 0, None)),
+                         ('units', WFDBheaderspecs([str], '/', 'adcgain', True, 'mV', None)),
+                         ('adcres', WFDBheaderspecs(inttypes, ' ', 'adcgain', False, None, 0)),
+                         ('adczero', WFDBheaderspecs(inttypes, ' ', 'adcres', False, None, 0)),
+                         ('initvalue', WFDBheaderspecs(inttypes, ' ', 'adczero', False, None, None)),
+                         ('checksum', WFDBheaderspecs(inttypes, ' ', 'initvalue', False, None, None)),
+                         ('blocksize', WFDBheaderspecs(inttypes, ' ', 'checksum', False, None, 0)),
+                         ('signame', WFDBheaderspecs([str], ' ', 'blocksize', False, None, None))])
+    
+# Segment specification fields.
+segfieldspecs = OrderedDict([('segname', WFDBheaderspecs([str], '', None, True, None, None)),
+                         ('seglen', WFDBheaderspecs(inttypes, ' ', 'segname', True, None, None))])
+
+
+
+
