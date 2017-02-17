0328b84df8ef447ac9d3bc1514f2662ffe73578f
Chen <is.chenx@gmail.com>
2017-02-17 09:06:22 -0500

    dev

--- wfdb/__init__.py
+++ wfdb/__init__.py
@@ -1,9 +1,8 @@
-#from ._rdann import rdann
 #from ._plotwfdb import plotwfdb
 #from ._downloadsamp import downloadsamp
 
 from .records import Record, MultiRecord, rdheader, rdsamp, srdsamp, swrsamp
+from .annotations import Annotation, rdann
+
 
 #from ._plot import plotrec
-
-#from .annotations import Annotation, rdann
