aa4452e3bec86f703cd0eeb24c818e7a146ee8a7
Jacob Svensson <jacob@nephics.com>
2017-06-15 16:00:59 +0200

    Use isinstance() for argument type checking

--- wfdb/plot/plots.py
+++ wfdb/plot/plots.py
@@ -160,9 +160,9 @@ def calc_ecg_grids(minsig, maxsig, units, fs, maxt, timeunits):
 def checkplotitems(record, title, annotation, annch, timeunits):
     
     # signals
-    if type(record) != records.Record:
+    if not isinstance(record, records.Record):
         raise TypeError("The 'record' argument must be a valid wfdb.Record object")
-    if type(record.p_signals) != np.ndarray or record.p_signals.ndim != 2:
+    if not isinstance(record.p_signals, np.ndarray) or record.p_signals.ndim != 2:
         raise TypeError("The plotted signal 'record.p_signals' must be a 2d numpy array")
     
     siglen, nsig = record.p_signals.shape
@@ -175,7 +175,7 @@ def checkplotitems(record, title, annotation, annch, timeunits):
     if timeunits == 'samples':
         t = np.linspace(0, siglen-1, siglen)
     else:
-        if type(record.fs) not in _headers.floattypes:
+        if not isinstance(record.fs, _headers.floattypes):
             raise TypeError("The 'fs' field must be a number")
         
         if timeunits == 'seconds':
@@ -189,7 +189,7 @@ def checkplotitems(record, title, annotation, annch, timeunits):
     if record.units is None:
         record.units = ['NU']*nsig
     else:
-        if type(record.units) != list or len(record.units)!= nsig:
+        if not isinstance(record.units, list) or len(record.units)!= nsig:
             raise ValueError("The 'units' parameter must be a list of strings with length equal to the number of signal channels")
         for ch in range(nsig):
             if record.units[ch] is None:
@@ -199,18 +199,18 @@ def checkplotitems(record, title, annotation, annch, timeunits):
     if record.signame is None:
         record.signame = ['ch'+str(ch) for ch in range(1, nsig+1)] 
     else:
-        if type(record.signame) != list or len(record.signame)!= nsig:
+        if not isinstance(record.signame, list) or len(record.signame)!= nsig:
             raise ValueError("The 'signame' parameter must be a list of strings, with length equal to the number of signal channels")
                 
     # title
-    if title is not None and type(title) != str:
+    if title is not None and not isinstance(title, str):
         raise TypeError("The 'title' field must be a string")
     
     # Annotations if any
     if annotation is not None:
-        if type(annotation) != annotations.Annotation:
+        if not isinstance(annotation, annotations.Annotation):
             raise TypeError("The 'annotation' argument must be a valid wfdb.Annotation object")
-        if type(annch)!= list:
+        if not isinstance(annch, list):
             raise TypeError("The 'annch' argument must be a list of integers")
         if min(annch)<0 or max(annch)>nsig:
             raise ValueError("The elements of 'annch' must be between 0 and the number of record channels")
@@ -284,7 +284,7 @@ def plotann(annotation, title = None, timeunits = 'samples', returnfig = False):
 def checkannplotitems(annotation, title, timeunits):
     
     # signals
-    if type(annotation)!= annotations.Annotation:
+    if not isinstance(annotation, annotations.Annotation):
         raise TypeError("The 'annotation' field must be a 'wfdb.Annotation' object")
 
     # fs and timeunits
@@ -294,7 +294,7 @@ def checkannplotitems(annotation, title, timeunits):
 
     # fs must be valid when plotting time
     if timeunits != 'samples':
-        if type(annotation.fs) not in _headers.floattypes:
+        if not isinstance(annotation.fs, _headers.floattypes):
             raise Exception("In order to plot time units, the Annotation object must have a valid 'fs' attribute")
 
     # Get x axis values to plot
@@ -308,7 +308,7 @@ def checkannplotitems(annotation, title, timeunits):
         plotvals = annotation.annsamp/(annotation.fs*3600)
 
     # title
-    if title is not None and type(title) != str:
+    if title is not None and not isinstance(title, str):
         raise TypeError("The 'title' field must be a string")
     
     return plotvals
