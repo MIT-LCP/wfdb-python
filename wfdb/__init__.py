f3c081ad63df963af2f6eb4746e367c61bf35d78
Lucas McCullum <lumccul3@gmail.com>
2021-03-31 13:37:28 -0400

    Cleans up ann2rr; adds rr2ann

--- wfdb/__init__.py
+++ wfdb/__init__.py
@@ -2,7 +2,7 @@ from wfdb.io.record import (Record, MultiRecord, rdheader, rdrecord, rdsamp,
                             wrsamp, dl_database, edf2mit, mit2edf, wav2mit, mit2wav,
                             wfdb2mat, csv2mit, sampfreq, signame)
 from wfdb.io.annotation import (Annotation, rdann, wrann, show_ann_labels,
-                                show_ann_classes, ann2rr)
+                                show_ann_classes, ann2rr, rr2ann)
 from wfdb.io.download import get_dbs, get_record_list, dl_files, set_db_index_url
 from wfdb.plot.plot import plot_items, plot_wfdb, plot_all_records
 
