608c2a3a443e221195b1873c616998fbaf1d997d
Lucas McCullum <lumccul3@gmail.com>
2021-04-02 12:55:21 -0400

    Adds wfdbtime function

--- wfdb/__init__.py
+++ wfdb/__init__.py
@@ -1,6 +1,6 @@
 from wfdb.io.record import (Record, MultiRecord, rdheader, rdrecord, rdsamp,
                             wrsamp, dl_database, edf2mit, mit2edf, wav2mit, mit2wav,
-                            wfdb2mat, csv2mit, sampfreq, signame, wfdbdesc)
+                            wfdb2mat, csv2mit, sampfreq, signame, wfdbdesc, wfdbtime)
 from wfdb.io.annotation import (Annotation, rdann, wrann, show_ann_labels,
                                 show_ann_classes, ann2rr, rr2ann)
 from wfdb.io.download import get_dbs, get_record_list, dl_files, set_db_index_url
