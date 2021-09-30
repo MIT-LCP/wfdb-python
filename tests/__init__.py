791a9a0521f790b215b221d841deaf35695d0660
Benjamin Moody <benjaminmoody@gmail.com>
2021-09-30 12:48:11 -0400

    When running nosetests, raise exceptions for arithmetic errors.
    nosetests will invoke the functions setup_module() and
    teardown_module(), if defined, before and after running all test cases
    within the module.
    
    In general, numpy arithmetic errors (e.g. integer or floating-point
    overflow, or division by zero) should not happen under normal
    circumstances, and the package should neither assume that these errors
    will raise an exception, nor assume that they won't.  Therefore, while
    running the test suite, set the numpy error handling mode to "raise"
    so the test will fail if such an error occurs.

---
+++ tests/__init__.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+_np_error_state = {}
+
+
+def setup_module():
+    # Raise exceptions for arithmetic errors, except underflow
+    global _np_error_state
+    _np_error_state = np.seterr()
+    np.seterr('raise', under='ignore')
+
+
+def teardown_module():
+    # Restore original error handling state
+    np.seterr(**_np_error_state)
